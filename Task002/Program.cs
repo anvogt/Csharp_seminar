80a331225fe01b83a6cdb9532cf2b4f55c325d3d
Anfisa Vogt <xnfisx@mail.ru>
2023-10-11 02:25:08 +0500

    Initial commit

---
+++ Task002/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 326791123 -> 6
+
+using System.Linq.Expressions;
+
+System.Console.WriteLine("Введите любое целое число");
+
+int N = Convert.ToInt32(Console.ReadLine());
+
+while (N > 1000)
+{
+    N = N / 10;
+}
+
+if (N < 100)
+{
+    System.Console.WriteLine("Третьей цифры нет");
+}
+
+else {
+    System.Console.WriteLine(N%10);
+}
